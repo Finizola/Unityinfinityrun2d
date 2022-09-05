3a5a929494d99b1668136b661c71e013e22bd7fe
Finizola <leo.finizola@gmail.com>
2022-09-05 10:26:34 -0300

    atualizando

--- Assets/Scripts/SpawnPlataform.cs
+++ Assets/Scripts/SpawnPlataform.cs
@@ -8,8 +8,13 @@ public class SpawnPlataform : MonoBehaviour
     Transform _transform;
     int contador = 0;
     bool proximo;
-    Transform p;
-    Collider2D colisao;
+    float offSet = 15;
+    float plataformX;
+    float deadEnd;
+    int respawContador = 1;
+
+
+
 
     void Start()
     {
@@ -24,26 +29,29 @@ public class SpawnPlataform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(proximo);
-        p = Instantiate(plataformsList[Random.Range(0, 4)], new Vector2(contador * 25, 0), transform.rotation).transform;
-        contador++;
-        Debug.Log(p);
-        //colisor2dBox = p.GetComponent<Collider2D>().b;
-        //Debug.Log(contador);
-
+        
         var playerX = Player.instance.transform.position.x;
-        var offSet = 15;
-        var deadEnd = playerX - offSet;
-        //var plataformX = transform.GetComponentInChildren<Collider2D>().bounds.max.x;
+        if (playerX > respawContador * 25)
+        {
+            var p = Instantiate(plataformsList[Random.Range(0, 4)], new Vector2(contador * 25, 0), transform.rotation).transform;
+            contador++;
+            respawContador++;
+        }
+       
+       
+            //plataformX = p.GetComponent<Collider2D>().bounds.max.x;
+            
+            deadEnd = playerX - offSet;
+            Debug.Log($"plataforma: {plataformX}\n player: {playerX}");
+            //Destroy(p, 1f);
+            //if (plataformX > deadEnd)
+            //{
+            //    Debug.Log("BOOM!");
 
-        //if (plataformX < deadEnd)
-        //{
-        //    //Debug.Log($"plataforma: {plataformX}\n player: {playerX}");
-        //    GameObject.Destroy(transform.gameObject);
-        //    proximo = true;
-        //    Debug.Log(proximo);
-        //}
-
+            //    proximo = true;
+            //    Debug.Log(proximo);
+            //}
+        
 
     }
 }
