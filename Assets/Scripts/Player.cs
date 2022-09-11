3ff2f9891bcb6afe83c61fc6e729f534e8dc9faf
Finizola <leo.finizola@gmail.com>
2022-09-11 13:03:44 -0300

    finizola

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,22 +7,54 @@ public class Player : MonoBehaviour
     private Rigidbody2D rig2D;
     private Animator animator2d;
     public static Player instance;
+
+    private bool isGround;
+    int puloDuplo;
+    private GameObject destroyTerrain;
     // Start is called before the first frame update
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.layer == 3)
+        {
+            isGround = true;
+            //puloDuplo = 2;
+        }
+    
+        if (collision.gameObject.CompareTag("TerrainA"))
+        {
+            destroyTerrain = GameObject.FindGameObjectWithTag("TerrainA");
+            Destroy(destroyTerrain, 4f);
+            Debug.Log("boom!");
+        }
+    }
+
     void Start()
     {
         instance = this;
         rig2D = GetComponent<Rigidbody2D>();
         animator2d = GetComponent<Animator>();
+        puloDuplo = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rig2D.velocity = new Vector2(6f, rig2D.velocity.y);
+        rig2D.velocity = new Vector2(5f, rig2D.velocity.y);
         
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && isGround == true)
         {
-            rig2D.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
-        }
+            
+           rig2D.AddForce(Vector2.up * 4, ForceMode2D.Impulse);
+           Debug.Log(puloDuplo);
+           puloDuplo--;
+           if (puloDuplo == 0 )
+                {
+                    isGround = false;
+                    Debug.Log("zerou");
+                    puloDuplo = 2;
+                }
+         }
+        
     }
 }
--- Assets/Scripts/SpawnPlataform.cs
+++ Assets/Scripts/SpawnPlataform.cs
@@ -14,56 +14,23 @@ public class SpawnPlataform : MonoBehaviour
 
 
 
-
-    void Start()
-    {
-        
-      
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         
         var playerX = Player.instance.transform.position.x;
-        if (playerX > respawContador * 17)
+        if (playerX >= respawContador * 17)
         {
-
+            //Debug.Log(playerX);
             var p = Instantiate(plataformsList[Random.Range(0, 4)], new Vector2(contador * 25, 0), transform.rotation).transform;
             var offSet = -playerX + p.GetComponent<BoxCollider2D>().bounds.max.x;
             contador++;
             respawContador++;
-            if(offSet > contador *15)
-            {
-                Destroy(p.gameObject, 10f);
-                Debug.Log("boom");
-            }
-
-            //Debug.Log($" BOUDS:{p.GetComponent<BoxCollider2D>().bounds.max.x}");
-            Debug.Log($"P: {p.transform.position.x}");
-            Debug.Log(offSet);
-            //offSet = p.transform.position.x - playerX;
-            //Debug.Log(offSet * 1.33);
-            //Debug.Log($"posicao do P: {p.transform.position.x} \n posicao do Player: {playerX}");
-            //Debug.Log($" contador:{contador} \n respawcontador:{respawContador}");
+           
+            //Debug.Log($"P: {p.transform.position.x}");
+            
         }
        
-       
-            //plataformX = p.GetComponent<Collider2D>().bounds.max.x;
-            
-            //deadEnd = playerX - offSet;
-            //Debug.Log($"plataforma: {plataformX}\n player: {playerX}");
-            //Destroy(p, 1f);
-            //if (plataformX > deadEnd)
-            //{
-            //    Debug.Log("BOOM!");
-
-            //    proximo = true;
-            //    Debug.Log(proximo);
-            //}
-        
-
     }
 }
 
